d215374326fba607966d1bcfdd56a68c36f10a6f
velmurugan012001 <bvelmurugan01122001@gmai.com>
2023-04-11 16:24:42 +0530

    first commit

---
+++ Food  portal-date and time/Food  portal-date and time/letsstart.js
@@ -0,0 +1,19 @@
+function popupfunc(){
+     var popup=document.getElementById("timepopup");
+     popup.classList.toggle("show");
+   
+}
+function showDiv() {
+    document.getElementById("demo").style.display = "block";
+}
+
+function changeColor(){
+    document.getElementById("btn1").style.backgroundColor= '#FF6D00';
+}
+
+
+
+
+
+
+   
---
+++ food_selection2/try2/scripts/script.js
@@ -0,0 +1,373 @@
+function myplate() 
+{
+    var x = document.getElementById("samosa1").checked;
+
+    var y = document.getElementById("onionpakora1").checked;
+
+    var z = document.getElementById("mixvegpakora1").checked;
+
+    var a = document.getElementById("cutmirchipakora1").checked;
+
+    var b = document.getElementById("gobhimanchurian1").checked;
+
+
+    if (x == '1') {
+        document.getElementById('samosa').style.display = 'block';
+        document.getElementById('onionpakora').style.display = 'none';
+        document.getElementById('mixvegpakora').style.display = 'none';
+        document.getElementById('cutmirchipakora').style.display = 'none';
+        document.getElementById('gobhimanchurian').style.display = 'none';
+    }
+    if (y == '1') {
+        document.getElementById('onionpakora').style.display = 'block';
+        document.getElementById('samosa').style.display = 'none';
+        document.getElementById('mixvegpakora').style.display = 'none';
+        document.getElementById('cutmirchipakora').style.display = 'none';
+        document.getElementById('gobhimanchurian').style.display = 'none';
+       
+    }
+    if (z == '1') {
+        document.getElementById('mixvegpakora').style.display = 'block';
+        document.getElementById('onionpakora').style.display = 'none';
+        document.getElementById('samosa').style.display = 'none';
+        document.getElementById('cutmirchipakora').style.display = 'none';
+        document.getElementById('gobhimanchurian').style.display = 'none';
+        
+    }
+    if (a == '1') {
+        document.getElementById('cutmirchipakora').style.display = 'block';
+        document.getElementById('samosa').style.display = 'none';
+        document.getElementById('gobhimanchurian').style.display = 'none';
+        document.getElementById('onionpakora').style.display = 'none';
+        document.getElementById('mixvegpakora').style.display = 'none';
+       
+    }
+    if (b == '1') {
+        document.getElementById('gobhimanchurian').style.display = 'block';
+        document.getElementById('samosa').style.display = 'none';
+        document.getElementById('cutmirchipakora').style.display = 'none';
+        document.getElementById('onionpakora').style.display = 'none';
+        document.getElementById('mixvegpakora').style.display = 'none';
+        
+    }
+   
+}
+
+function myplate2() 
+{
+    var x = document.getElementById("samosa12").checked;
+
+    var y = document.getElementById("onionpakora12").checked;
+
+    var z = document.getElementById("mixvegpakora12").checked;
+
+    var a = document.getElementById("cutmirchipakora12").checked;
+
+    var b = document.getElementById("gobhimanchurian12").checked;
+
+
+    if (x == '1') {
+        document.getElementById('samosaa').style.display = 'block';
+        document.getElementById('onionpakoraa').style.display = 'none';
+        document.getElementById('mixvegpakoraa').style.display = 'none';
+        document.getElementById('cutmirchipakoraa').style.display = 'none';
+        document.getElementById('gobhimanchuriana').style.display = 'none';
+    }
+    if (y == '1') {
+        document.getElementById('onionpakoraa').style.display = 'block';
+        document.getElementById('samosaa').style.display = 'none';
+        document.getElementById('mixvegpakoraa').style.display = 'none';
+        document.getElementById('cutmirchipakoraa').style.display = 'none';
+        document.getElementById('gobhimanchuriana').style.display = 'none';
+       
+    }
+    if (z == '1') {
+        document.getElementById('mixvegpakoraa').style.display = 'block';
+        document.getElementById('onionpakoraa').style.display = 'none';
+        document.getElementById('samosaa').style.display = 'none';
+        document.getElementById('cutmirchipakoraa').style.display = 'none';
+        document.getElementById('gobhimanchuriana').style.display = 'none';
+        
+    }
+    if (a == '1') {
+        document.getElementById('cutmirchipakoraa').style.display = 'block';
+        document.getElementById('samosaa').style.display = 'none';
+        document.getElementById('gobhimanchuriana').style.display = 'none';
+        document.getElementById('onionpakoraa').style.display = 'none';
+        document.getElementById('mixvegpakoraa').style.display = 'none';
+       
+    }
+    if (b == '1') {
+        document.getElementById('gobhimanchuriana').style.display = 'block';
+        document.getElementById('samosaa').style.display = 'none';
+        document.getElementById('cutmirchipakoraa').style.display = 'none';
+        document.getElementById('onionpakoraa').style.display = 'none';
+        document.getElementById('mixvegpakoraa').style.display = 'none';
+        
+    }
+   
+}
+
+function myplate3() 
+{
+    var x = document.getElementById("samosa13").checked;
+
+    var y = document.getElementById("onionpakora13").checked;
+
+    var z = document.getElementById("mixvegpakora13").checked;
+
+    var a = document.getElementById("cutmirchipakora13").checked;
+
+    var b = document.getElementById("gobhimanchurian13").checked;
+
+
+    if (x == '1') {
+        document.getElementById('samosab').style.display = 'block';
+        document.getElementById('onionpakorab').style.display = 'none';
+        document.getElementById('mixvegpakorab').style.display = 'none';
+        document.getElementById('cutmirchipakorab').style.display = 'none';
+        document.getElementById('gobhimanchurianb').style.display = 'none';
+    }
+    if (y == '1') {
+        document.getElementById('onionpakorab').style.display = 'block';
+        document.getElementById('samosab').style.display = 'none';
+        document.getElementById('mixvegpakorab').style.display = 'none';
+        document.getElementById('cutmirchipakorab').style.display = 'none';
+        document.getElementById('gobhimanchurianb').style.display = 'none';
+       
+    }
+    if (z == '1') {
+        document.getElementById('mixvegpakorab').style.display = 'block';
+        document.getElementById('onionpakorab').style.display = 'none';
+        document.getElementById('samosab').style.display = 'none';
+        document.getElementById('cutmirchipakorab').style.display = 'none';
+        document.getElementById('gobhimanchurianb').style.display = 'none';
+        
+    }
+    if (a == '1') {
+        document.getElementById('cutmirchipakorab').style.display = 'block';
+        document.getElementById('samosab').style.display = 'none';
+        document.getElementById('gobhimanchurianb').style.display = 'none';
+        document.getElementById('onionpakorab').style.display = 'none';
+        document.getElementById('mixvegpakorab').style.display = 'none';
+       
+    }
+    if (b == '1') {
+        document.getElementById('gobhimanchurianb').style.display = 'block';
+        document.getElementById('samosab').style.display = 'none';
+        document.getElementById('cutmirchipakorab').style.display = 'none';
+        document.getElementById('onionpakorab').style.display = 'none';
+        document.getElementById('mixvegpakorab').style.display = 'none';
+        
+    }
+   
+}
+
+
+function myplate4() 
+{
+    var x = document.getElementById("samosa14").checked;
+
+    var y = document.getElementById("onionpakora14").checked;
+
+    var z = document.getElementById("mixvegpakora14").checked;
+
+    var a = document.getElementById("cutmirchipakora14").checked;
+
+    var b = document.getElementById("gobhimanchurian14").checked;
+
+
+    if (x == '1') {
+        document.getElementById('samosac').style.display = 'block';
+        document.getElementById('onionpakorac').style.display = 'none';
+        document.getElementById('mixvegpakorac').style.display = 'none';
+        document.getElementById('cutmirchipakorac').style.display = 'none';
+        document.getElementById('gobhimanchurianc').style.display = 'none';
+    }
+    if (y == '1') {
+        document.getElementById('onionpakorac').style.display = 'block';
+        document.getElementById('samosac').style.display = 'none';
+        document.getElementById('mixvegpakorac').style.display = 'none';
+        document.getElementById('cutmirchipakorac').style.display = 'none';
+        document.getElementById('gobhimanchurianc').style.display = 'none';
+       
+    }
+    if (z == '1') {
+        document.getElementById('mixvegpakorac').style.display = 'block';
+        document.getElementById('onionpakorac').style.display = 'none';
+        document.getElementById('samosac').style.display = 'none';
+        document.getElementById('cutmirchipakorac').style.display = 'none';
+        document.getElementById('gobhimanchurianc').style.display = 'none';
+        
+    }
+    if (a == '1') {
+        document.getElementById('cutmirchipakorac').style.display = 'block';
+        document.getElementById('samosac').style.display = 'none';
+        document.getElementById('gobhimanchurianc').style.display = 'none';
+        document.getElementById('onionpakorac').style.display = 'none';
+        document.getElementById('mixvegpakorac').style.display = 'none';
+       
+    }
+    if (b == '1') {
+        document.getElementById('gobhimanchurianc').style.display = 'block';
+        document.getElementById('samosac').style.display = 'none';
+        document.getElementById('cutmirchipakorac').style.display = 'none';
+        document.getElementById('onionpakorac').style.display = 'none';
+        document.getElementById('mixvegpakorac').style.display = 'none';
+        
+    }
+   
+}
+
+
+function myplate5() 
+{
+    var x = document.getElementById("samosa15").checked;
+
+    var y = document.getElementById("onionpakora15").checked;
+
+    var z = document.getElementById("mixvegpakora15").checked;
+
+    var a = document.getElementById("cutmirchipakora15").checked;
+
+    var b = document.getElementById("gobhimanchurian15").checked;
+
+
+    if (x == '1') {
+        document.getElementById('samosad').style.display = 'block';
+        document.getElementById('onionpakorad').style.display = 'none';
+        document.getElementById('mixvegpakorad').style.display = 'none';
+        document.getElementById('cutmirchipakorad').style.display = 'none';
+        document.getElementById('gobhimanchuriand').style.display = 'none';
+    }
+    if (y == '1') {
+        document.getElementById('onionpakorad').style.display = 'block';
+        document.getElementById('samosad').style.display = 'none';
+        document.getElementById('mixvegpakorad').style.display = 'none';
+        document.getElementById('cutmirchipakorad').style.display = 'none';
+        document.getElementById('gobhimanchuriand').style.display = 'none';
+       
+    }
+    if (z == '1') {
+        document.getElementById('mixvegpakorad').style.display = 'block';
+        document.getElementById('onionpakorad').style.display = 'none';
+        document.getElementById('samosad').style.display = 'none';
+        document.getElementById('cutmirchipakorad').style.display = 'none';
+        document.getElementById('gobhimanchuriand').style.display = 'none';
+        
+    }
+    if (a == '1') {
+        document.getElementById('cutmirchipakorad').style.display = 'block';
+        document.getElementById('samosad').style.display = 'none';
+        document.getElementById('gobhimanchuriand').style.display = 'none';
+        document.getElementById('onionpakorad').style.display = 'none';
+        document.getElementById('mixvegpakorad').style.display = 'none';
+       
+    }
+    if (b == '1') {
+        document.getElementById('gobhimanchuriand').style.display = 'block';
+        document.getElementById('samosad').style.display = 'none';
+        document.getElementById('cutmirchipakorad').style.display = 'none';
+        document.getElementById('onionpakorad').style.display = 'none';
+        document.getElementById('mixvegpakorad').style.display = 'none';
+        
+    }
+   
+}
+
+
+var count = 1;
+function nextbody() {
+    count++;
+    
+    if(count == 1)
+    {
+        document.getElementById('changing_body11').style.display = 'block';
+        document.getElementById('changing_body12').style.display = 'none';
+        document.getElementById('changing_body13').style.display = 'none';
+        document.getElementById('changing_body14').style.display = 'none';
+        document.getElementById('changing_body15').style.display = 'none';
+    }
+    if(count == 2)
+    {
+        document.getElementById('changing_body11').style.display = 'none';
+        document.getElementById('changing_body12').style.display = 'block';
+        document.getElementById('changing_body13').style.display = 'none';
+        document.getElementById('changing_body14').style.display = 'none';
+        document.getElementById('changing_body15').style.display = 'none';
+    }
+    if(count == 3)
+    {
+        document.getElementById('changing_body11').style.display = 'none';
+        document.getElementById('changing_body12').style.display = 'none';
+        document.getElementById('changing_body13').style.display = 'block';
+        document.getElementById('changing_body14').style.display = 'none';
+        document.getElementById('changing_body15').style.display = 'none';
+    }
+    if(count == 4)
+    {
+        document.getElementById('changing_body11').style.display = 'none';
+        document.getElementById('changing_body12').style.display = 'none';
+        document.getElementById('changing_body13').style.display = 'none';
+        document.getElementById('changing_body14').style.display = 'block';
+        document.getElementById('changing_body15').style.display = 'none';
+    }
+    if(count == 5)
+    {
+        document.getElementById('changing_body11').style.display = 'none';
+        document.getElementById('changing_body12').style.display = 'none';
+        document.getElementById('changing_body13').style.display = 'none';
+        document.getElementById('changing_body14').style.display = 'none';
+        document.getElementById('changing_body15').style.display = 'block';
+    }
+    if(count == 6) 
+    {
+        window.open('page2.html')
+    }
+
+
+}
+
+function previousbody() {
+    count--;
+    if(count == 1)
+    {
+        document.getElementById('changing_body11').style.display = 'block';
+        document.getElementById('changing_body12').style.display = 'none';
+        document.getElementById('changing_body13').style.display = 'none';
+        document.getElementById('changing_body14').style.display = 'none';
+        document.getElementById('changing_body15').style.display = 'none';
+    }
+    if(count == 2)
+    {
+        document.getElementById('changing_body11').style.display = 'none';
+        document.getElementById('changing_body12').style.display = 'block';
+        document.getElementById('changing_body13').style.display = 'none';
+        document.getElementById('changing_body14').style.display = 'none';
+        document.getElementById('changing_body15').style.display = 'none';
+    }
+    if(count == 3)
+    {
+        document.getElementById('changing_body11').style.display = 'none';
+        document.getElementById('changing_body12').style.display = 'none';
+        document.getElementById('changing_body13').style.display = 'block';
+        document.getElementById('changing_body14').style.display = 'none';
+        document.getElementById('changing_body15').style.display = 'none';
+    }
+    if(count == 4)
+    {
+        document.getElementById('changing_body11').style.display = 'none';
+        document.getElementById('changing_body12').style.display = 'none';
+        document.getElementById('changing_body13').style.display = 'none';
+        document.getElementById('changing_body14').style.display = 'block';
+        document.getElementById('changing_body15').style.display = 'none';
+    }
+    if(count == 5)
+    {   
+        document.getElementById('changing_body11').style.display = 'none';
+        document.getElementById('changing_body12').style.display = 'none';
+        document.getElementById('changing_body13').style.display = 'none';
+        document.getElementById('changing_body14').style.display = 'none';
+        document.getElementById('changing_body15').style.display = 'block';
+    }
+}
